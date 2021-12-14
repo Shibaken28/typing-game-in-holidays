1388a20b29bc5b76dfddb822a12a5b0558c01af4
Shibaken28 <8128kanzensu@gmail.com>
2021-12-14 15:34:23 +0900

    update

--- sketch.js
+++ sketch.js
@@ -5,13 +5,18 @@ var t=0;
 var nowChar=0;
 var song;
 var count =0;
+var highScore=99999;
+var start=0;
+var playing=0;
+var time=99999;
 
 /*function preload() {
 song = loadSound('type.mp3');
 }*/
 
 function setup() {
-  createCanvas(800, 800);
+  start=millis();
+  createCanvas(windowWidth, windowHeight);
   textAlign(CENTER, CENTER);
   textFont("Sawarabi Mincho");
 }
@@ -24,20 +29,20 @@ function textEffect(s,x,y,w,op){
         fill(30);
         stroke(10);
         strokeWeight(3);
-        text(s.charAt(i),x+dx/2-w/2+dx*i,y);
+        text(s.charAt(i),x+dx/2-w/2+dx*i+noise(t/10,i,0.5)*15,y+noise(t/10,i,0)*15);
       }else{
         fill(200);
         stroke(10);
         strokeWeight(3);
-        text(s.charAt(i),x+dx/2-w/2+dx*i+noise(t/10,i,0.5)*7,y+noise(t/10,i,0)*7);
+        text(s.charAt(i),x+dx/2-w/2+dx*i+noise(t/10,i,0.5)*15,y+noise(t/10,i,0)*15);
       }
     }else{
       noStroke();
       fill(150);
-      text(s.charAt(i),x+dx/2-w/2+dx*i+noise(t/10.0,i,0.5)*7+3,y+noise(t/10.0,i,0)*7+3);
+      text(s.charAt(i),x+dx/2-w/2+dx*i+noise(t/10.0,i,0.5)*15+3,y+noise(t/10.0,i,0)*15+3);
       noStroke();
       fill(20);
-      text(s.charAt(i),x+dx/2-w/2+dx*i+noise(t/10.0,i,0.5)*7,y+noise(t/10.0,i,0)*7);
+      text(s.charAt(i),x+dx/2-w/2+dx*i+noise(t/10.0,i,0.5)*15,y+noise(t/10.0,i,0)*15);
     }
   }
 }
@@ -54,13 +59,11 @@ function draw() {
   t+=1;
   background(220);
   drawBack();
-  textSize(130);
-  textEffect(kanjiText,width/2,460,600,0);
-  textSize(80);
-  textEffect(typeText,width/2,310,600,1);
-  textSize(30);
-  fill(200);
-  textShadow("Number of 有給休暇:"+count,200,30,3,3);
+  if(playing==1){
+    play();
+  }else{
+    menu();
+  }
 }
 
 function drawBack(){
@@ -84,19 +87,63 @@ function drawBack(){
   }
 }
 
+function menu(){
+  start=millis();
+  textSize(100);
+  textAlign(CENTER, CENTER);
+  textShadow("Last Score :  "+floorU(time/10000,100)+" sec/有給休暇",width/2,height/2-100,3,3);
+  textShadow("Best Score :  "+floorU(highScore/10000,100)+" sec/有給休暇",width/2,height/2+100,3,3);
+}
+
+
+function floorU(a,b){
+  return Math.floor(a*b)/b;
+}
+
+function play(){
+  textSize(300);
+  textAlign(CENTER, CENTER);
+  textEffect(kanjiText,width/2,height/2+110,1200,0);
+  textSize(120);
+  textEffect(typeText,width/2,height/2-100,1200,1);
+  textSize(100);
+  fill(200);
+  textAlign(LEFT, LEFT);
+  textShadow("Number of 有給休暇:"+count,20,height/3+130,3,3);
+}
+
 
 function keyPressed(){  
+  if(playing==0){
+    if(key=="Enter"){
+      nowChar=0;
+      count=0;
+      playing=1;
+    }
+    return;
+  }
+  if(key=="Escape"){
+    playing=0;
+  }
   if(nowChar==typeText.length){
     if(key=="Enter"){
       nowChar=0;
       count++;
-      //song.play();
+      if(count>=10){
+        time=millis()-start;
+        if(highScore>time)highScore=time;
+        playing=0;
+      }
+      song.play();
     }
   }else{
     if(key==typeText.charAt(nowChar)){
       nowChar++;
-      //song.play();
+      song.play();
     }
   }
 }
 
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
