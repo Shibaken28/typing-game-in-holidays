595e8ae14c186ccdcd7eb20c1f9cf69d8ca40091
Shibaken28 <8128kanzensu@gmail.com>
2021-12-14 11:31:54 +0900

    update

--- sketch.js
+++ sketch.js
@@ -6,9 +6,9 @@ var nowChar=0;
 var song;
 var count =0;
 
-function preload() {
-  song = loadSound('type.mp3');
-}
+/*function preload() {
+song = loadSound('type.mp3');
+}*/
 
 function setup() {
   createCanvas(800, 800);
@@ -29,41 +29,73 @@ function textEffect(s,x,y,w,op){
         fill(200);
         stroke(10);
         strokeWeight(3);
-        text(s.charAt(i),x+dx/2-w/2+dx*i+noise(t,i,0.5)*7,y+noise(t,i,0)*7);
+        text(s.charAt(i),x+dx/2-w/2+dx*i+noise(t/10,i,0.5)*7,y+noise(t/10,i,0)*7);
       }
     }else{
       noStroke();
-      fill(80);
-      text(s.charAt(i),x+dx/2-w/2+dx*i+noise(t,i,0.5)*7+3,y+noise(t,i,0)*7+3);
+      fill(150);
+      text(s.charAt(i),x+dx/2-w/2+dx*i+noise(t/10.0,i,0.5)*7+3,y+noise(t/10.0,i,0)*7+3);
       noStroke();
       fill(20);
-      text(s.charAt(i),x+dx/2-w/2+dx*i+noise(t,i,0.5)*7,y+noise(t,i,0)*7);
+      text(s.charAt(i),x+dx/2-w/2+dx*i+noise(t/10.0,i,0.5)*7,y+noise(t/10.0,i,0)*7);
     }
   }
 }
 
-function draw() {
-  t+=0.15;
-  background(220);
-  textSize(130);
-  textEffect(kanjiText,width/2,470,600,0);
-  textSize(80);
-  textEffect(typeText,width/2,330,600,1);
-  textSize(30);
-  textEffect("Number of yuukyuukyuuka:"+count,200,30,300,0);
+function textShadow(s,x,y,dx,dy){
+  noStroke();
+  fill(200);
+  text(s,x+dx,y+dy);
+  fill(90);
+  text(s,x,y);
 }
+
+function draw() {
+  t+=1;
+  background(220);
+  drawBack();
+  textSize(130);
+  textEffect(kanjiText,width/2,460,600,0);
+  textSize(80);
+  textEffect(typeText,width/2,310,600,1);
+  textSize(30);
+  fill(200);
+  textShadow("Number of 有給休暇:"+count,200,30,3,3);
+}
+
+function drawBack(){
+  stroke(10);
+  strokeWeight(2);
+  for(var theta=0-(t%20)/100;theta<PI/2;theta+=0.2){
+    var y=height-sin(theta)*height/3;
+    line(0,y,width,y);
+  }
+  line(0,height/3*2,width,height/3*2);
+  for(var x=-width*3;x<width*3;x+=133){
+    line(width/2+(x-width/2)*0.3,height/3*2,x,height);
+  }
+  for(var theta=0-(t%20)/100;theta<PI/2;theta+=0.2){
+    var y=sin(theta)*height/3;
+    line(0,y,width,y);
+  }
+  line(0,height/3,width,height/3);
+  for(var x=-width*3;x<width*3;x+=133){
+    line(width/2+(x-width/2)*0.3,height/3,x,0);
+  }
+}
+
 
 function keyPressed(){  
   if(nowChar==typeText.length){
     if(key=="Enter"){
       nowChar=0;
       count++;
-      song.play();
+      //song.play();
     }
   }else{
     if(key==typeText.charAt(nowChar)){
       nowChar++;
-      song.play();
+      //song.play();
     }
   }
 }
